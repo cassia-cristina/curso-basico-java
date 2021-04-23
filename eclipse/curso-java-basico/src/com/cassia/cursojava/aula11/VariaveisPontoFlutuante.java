4fa95271d0693d40e4a0a94341f4dbac8c5e7231
cassia-cristina <cassia.cristina.go@gmail.com>
2021-04-23 00:16:48 -0300

    Novas lições do curso

--- eclipse/curso-java-basico/src/com/cassia/cursojava/aula11/VariaveisPontoFlutuante.java
+++ eclipse/curso-java-basico/src/com/cassia/cursojava/aula11/VariaveisPontoFlutuante.java
@@ -1,14 +1,22 @@
 package com.cassia.cursojava.aula11;
+import java.text.DecimalFormat;
 
 public class VariaveisPontoFlutuante {
 	
 	public static void main (String[] args) {
 	
-		double valorPassagem = 4.10;
+		double valorPassagem = 123.4;
+		//usando notação científica
+		double valorPassagem2 = 1.234e2;
 		float valorTomate = 6.17f;
+		
+		//Usando formatação de casas decimais
+		DecimalFormat df = new DecimalFormat("#,###.00");
 	
-		System.out.println("Valor da passagem: " + valorPassagem);
-		System.out.println("Valor do kilo do tomate: " + valorTomate);
+		System.out.println("Valor da passagem: " + df.format(valorPassagem));
+		System.out.print("Valor da passagem: " + df.format(valorPassagem2) + "\n\r");
+		System.out.println("Valor do \"kilo\" do tomate: " + valorTomate);
+		
 	}
 
 }
