d3461937def01fb85c82ad0db03c942fb92b9424
cassia-cristina <cassia.cristina.go@gmail.com>
2021-05-15 15:21:28 -0300

    Novas licoes

---
+++ src/com/cassia/cursojava/licoes/aula20/ArrayFibonacci.java
@@ -0,0 +1,27 @@
+package com.cassia.cursojava.licoes.aula20;
+
+public class ArrayFibonacci {
+	
+	public static void main(String[] args) {
+
+		int[] vetor = new int[20];
+	
+		vetor[0] = 1;
+		vetor[1] = 1;
+		
+		System.out.print(vetor[0] + " ");
+		System.out.print(vetor[1] + " ");
+		
+		//os próximo é a soma dos dois anteriores
+		
+		for(int i=2; i<vetor.length; i++) {
+			
+			vetor[i] = (vetor[i-2]) + (vetor[i-1]); 
+			System.out.print(vetor[i] + " ");
+		}
+	
+	
+	}
+	
+	
+}
